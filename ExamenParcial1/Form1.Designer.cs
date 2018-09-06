6b0832de985cfc1ca4849e16a13b75081a871543
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-09-06 09:49:35 -0700

    Agregar archivos de proyecto.

---
+++ ExamenParcial1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ExamenParcial1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rojoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cannyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgImagen = new Emgu.CV.UI.ImageBox();
+            this.htgRojo = new Emgu.CV.UI.HistogramBox();
+            this.imgImagen2 = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImagen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImagen2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.histogramaToolStripMenuItem,
+            this.filtroToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1474, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // histogramaToolStripMenuItem
+            // 
+            this.histogramaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rojoToolStripMenuItem});
+            this.histogramaToolStripMenuItem.Name = "histogramaToolStripMenuItem";
+            this.histogramaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.histogramaToolStripMenuItem.Text = "Histograma";
+            // 
+            // filtroToolStripMenuItem
+            // 
+            this.filtroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cannyToolStripMenuItem});
+            this.filtroToolStripMenuItem.Name = "filtroToolStripMenuItem";
+            this.filtroToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.filtroToolStripMenuItem.Text = "Filtro";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // rojoToolStripMenuItem
+            // 
+            this.rojoToolStripMenuItem.Name = "rojoToolStripMenuItem";
+            this.rojoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rojoToolStripMenuItem.Text = "Rojo";
+            this.rojoToolStripMenuItem.Click += new System.EventHandler(this.rojoToolStripMenuItem_Click);
+            // 
+            // cannyToolStripMenuItem
+            // 
+            this.cannyToolStripMenuItem.Name = "cannyToolStripMenuItem";
+            this.cannyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cannyToolStripMenuItem.Text = "Canny";
+            this.cannyToolStripMenuItem.Click += new System.EventHandler(this.cannyToolStripMenuItem_Click);
+            // 
+            // imgImagen
+            // 
+            this.imgImagen.Location = new System.Drawing.Point(13, 28);
+            this.imgImagen.Name = "imgImagen";
+            this.imgImagen.Size = new System.Drawing.Size(425, 410);
+            this.imgImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgImagen.TabIndex = 2;
+            this.imgImagen.TabStop = false;
+            // 
+            // htgRojo
+            // 
+            this.htgRojo.Location = new System.Drawing.Point(445, 28);
+            this.htgRojo.Name = "htgRojo";
+            this.htgRojo.Size = new System.Drawing.Size(444, 410);
+            this.htgRojo.TabIndex = 3;
+            // 
+            // imgImagen2
+            // 
+            this.imgImagen2.Location = new System.Drawing.Point(896, 28);
+            this.imgImagen2.Name = "imgImagen2";
+            this.imgImagen2.Size = new System.Drawing.Size(460, 410);
+            this.imgImagen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgImagen2.TabIndex = 2;
+            this.imgImagen2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1474, 834);
+            this.Controls.Add(this.imgImagen2);
+            this.Controls.Add(this.htgRojo);
+            this.Controls.Add(this.imgImagen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImagen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImagen2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem histogramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rojoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cannyToolStripMenuItem;
+        private Emgu.CV.UI.ImageBox imgImagen;
+        private Emgu.CV.UI.HistogramBox htgRojo;
+        private Emgu.CV.UI.ImageBox imgImagen2;
+    }
+}
+
---
+++ ExamenParcial1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ExamenParcial1
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> inputImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    inputImage = new Image<Bgr, byte>(ofd.FileName);
+                    imgImagen.Image = inputImage;
+                    imgImagen.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void rojoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                return;
+            }
+
+            htgRojo.ClearHistogram();
+
+            DenseHistogram histRojo = new DenseHistogram(256, new RangeF(0, 255));
+            histRojo.Calculate(new Image<Gray, byte>[] { inputImage[0] }, false, null);
+
+            Mat m = new Mat();
+            histRojo.CopyTo(m);
+
+            htgRojo.AddHistogram("Histograma Rojo", Color.Red, m, 256, new float[] { 2, 255 });
+            htgRojo.Refresh();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Estas seguro que deseas salir?", "System Message", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void cannyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (inputImage == null)
+            {
+                return;
+            }
+
+            Image<Gray, byte> imgCanny = new Image<Gray, byte>(inputImage.Width, inputImage.Height, new Gray(0));
+            imgCanny = inputImage.Canny(50, 30);
+            imgImagen2.Image = imgCanny;
+            imgImagen2.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+        }
+    }
+}
